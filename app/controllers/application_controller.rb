bb690adf01c059e499caedfc20967f8ef4bc7cf0
calebcox725 <caleb.cox@flatironschool.com>
2016-10-16 14:17:41 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,49 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  get '/' do
+
+  get '/recipes' do
+    @recipes = Recipe.all
     erb :index
+  end
+
+  post '/recipes' do
+    new_recipe = Recipe.create(
+      name: params[:name],
+      ingredients: params[:ingredients],
+      cook_time: params[:cook_time]
+      )
+    redirect to "/recipes/#{new_recipe.id}"
+  end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    Recipe.update(
+      params[:id],
+      name: params[:name],
+      ingredients: params[:ingredients],
+      cook_time: params[:cook_time]
+      )
+
+    redirect to "/recipes/#{params[:id]}"
+  end
+
+  delete '/recipes/:id/delete' do
+    Recipe.destroy(params[:id])
+
+    redirect to "/recipes"
   end
 end
